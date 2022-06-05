eb0149bb911d5e6a8e3f12c8d3b3b260c2a72e63
Sharad Malik <sharadmalik1970@gmail.com>
2022-06-05 12:38:52 +0530

    Add one-to-many association between users and articles from back end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,63 +1,53 @@
 class ArticlesController < ApplicationController
- 
-before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-    def show
-     # @article = Article.find(params[:id])
-    end
-
-    def index
-      @articles = Article.all 
-      
-    end
-    
-    def new
-      @article = Article.new
-    end
-
-    def edit
-                         
-      #@article = Article.find(params[:id])
-    end
-
-    def create
-          
-      @article = Article.new(article_params)  #(params.require(:article).permit(:title, :description))
-
-     if @article.save
-       flash[:notice] = "Article was created sucessfully."
-
-      #redirect_to article_path(@article)# There is shot cut for this also
-       redirect_to @article
-        else
-          render 'new' #render the new form again
-        end
-
-    end
-       def update
-       # @article = Article.find(params[:id])
-        if @article.update(article_params)  #(params.require(:article).permit(:title, :description)) 
-           flash[:notice] = "Article was updated sucessfully." #very similar to the create action
-           redirect_to@article
-        else
-          render 'edit'
-          
-       end
+  def show
   end
-       def destroy
-        #  @article = Article.find(params[:id])
-          @article.destroy
-          redirect_to articles_path
-       end
+
+  def index
+    @articles = Article.all 
+  end
+  
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = User.first  
+    if @article.save
+      flash[:notice] = "Article was created sucessfully."
+      redirect_to @article
+    else
+      render 'new' 
+    end
+  end
+      
+  def update
+    if @article.update(article_params)  
+        flash[:notice] = "Article was updated sucessfully." 
+        redirect_to@article
+    else
+        render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
 
 end
 
-       private
+private
 
-       def set_article
-       @article = Article.find(params[:id])
-       end
+def set_article
+  @article = Article.find(params[:id])
+end
 
-       def article_params
-        params.require(:article).permit(:title, :description)
-       end
+def article_params
+  params.require(:article).permit(:title, :description)
+end
